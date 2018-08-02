300dd9d7c558628c9a779dfce59b2ebecc809184
Fatma.s.gadelrab <f_s_g2001@yahoo.co.uk>
2018-08-02 04:43:10 +0200

    Add files via upload

---
+++ config.ini.php
@@ -0,0 +1,9 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+[database]
+host                 = localhost
+username             = id6663956_root
+password             = root1
+dbname               = id6663956_categories_db
+port                 = 3306
+adapter              = PDO_MYSQL ; PDO_MYSQL or MYSQLI
+
